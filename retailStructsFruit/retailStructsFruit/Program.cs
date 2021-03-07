70e8d8ca5c8b440ad88835b8d172313c8822b16d
RobvanEmbden <61789232+RobvanEmbden@users.noreply.github.com>
2021-03-07 17:37:43 +0100

    Add files via upload

---
+++ retailStructsFruit/retailStructsFruit/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace retailStructsFruit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Coords c1 = new Coords();
+            c1.x = 100;
+            c1.y = 500;
+            Coords c2 = c1; // c2 can be used with effecting interference from c1
+            Console.WriteLine(c2.x + c2.y);
+        }
+        public struct Coords//refernce the object in the main method
+        {// many variables can be used, now 'x & y ' but can be as many as possible
+            public double x, y;
+        }
+    }
+}
